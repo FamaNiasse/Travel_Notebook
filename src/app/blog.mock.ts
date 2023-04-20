a42f78a3bb4453f03e5ae35082f8dcd1740e6d77
FamaNiasse <famasimplon@gmail.com>
2023-04-20 11:47:44 +0200

    Create component otherArticle but the display is not good for the moment will finalise it

--- src/app/blog.mock.ts
+++ src/app/blog.mock.ts
@@ -40,7 +40,7 @@ export const NOTEBOOK: INotebook[] = [
         "Prenez une paire de tong !"
     ],
     resumeContent: 'L’Albanie est un pays que nous avons adoré visiter, en grande partie grâce à l’accueil chaleureux des Albanais, mais pas seulement ! Nous avons découvert une culture fièrement préservée, une variété de paysages captivante et de nombreux coins tranquilles, y compris au mois de juillet. Seule la côte adriatique sature de touristes l’été. Notre conseil, si vous venez à cette saison, serait alors de s’éloigner de la mer et de privilégier les villages et les montagnes dans le reste du pays, où se concentre à notre avis tout le charme de l’Albanie.',
-    IsDisplay: false
+    IsDisplay: true
 },
 
 {
@@ -98,6 +98,30 @@ export const NOTEBOOK: INotebook[] = [
     id: 4,
     country: 'Tanzania',
     city: 'Zanzibar',
+    dateGo: new Date(2022, 4, 14),
+    dateBack: new Date(2022, 5, 17),
+    firstPicture: 'assets/img/first-pics.png',
+    otherPicture: [
+        'assets/img/other-pics.png'
+    ],
+    travelContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor massa id sem iaculis, et rutrum ligula ornare. Etiam malesuada diam justo, nec sagittis ligula rhoncus sit amet. Fusce ac viverra nisi. Integer nibh urna, posuere euismod dolor ac. Donec commodo pellentesque sapien eu interdum.Vestibulum neque nulla, mattis eget hendrerit nec, tempus pharetra est.Nulla facilisi.Curabitur elit lorem, rhoncus quis quam et, imperdiet euismod mi.Morbi eget elit ac velit porttitor sodales.Nulla scelerisque risus ex, a faucibus est eleifend sed.Aliquam ac facilisis lorem, ut ornare velit.dignissim metus.Aliquam dignissim lorem id nisi aliquet, a sodales ipsum dignissim.',
+    positive: [
+        "Un pays très simple à sillonner, sans embrouilles ni complexité. Des habitants gentils, spontanés et qui cherchent le contact. Un relief vallonné, offrant de superbes paysages dotés de villages pittoresques"
+    ],
+    negative: [
+        "Le climat torride en juillet-août, sauf en montagne. La côte très fréquentée l’été et de plus en plus bétonnée"
+    ],
+    advise: [
+        "Prenez une paire de tong !"
+    ],
+    resumeContent: 'L’Albanie est un pays que nous avons adoré visiter, en grande partie grâce à l’accueil chaleureux des Albanais, mais pas seulement ! Nous avons découvert une culture fièrement préservée, une variété de paysages captivante et de nombreux coins tranquilles, y compris au mois de juillet. Seule la côte adriatique sature de touristes l’été. Notre conseil, si vous venez à cette saison, serait alors de s’éloigner de la mer et de privilégier les villages et les montagnes dans le reste du pays, où se concentre à notre avis tout le charme de l’Albanie.',
+    IsDisplay: false
+},
+
+{
+    id: 5,
+    country: 'Kenya',
+    city: 'Nairobi',
     dateGo: new Date(2022, 4, 14),
     dateBack: new Date(2022, 5, 17),
     firstPicture: 'assets/img/first-pics.png',
